844498e96242bd97b15d318631dd21f957f813bd
Andrew Kirwin <andrew.kirwin@zestia.com>
2015-04-17 11:26:00 +0100

    initial commit

---
+++ app/controllers/index.js
@@ -0,0 +1,48 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+import BufferedMixin from 'ember-buffered-proxy/mixin';
+import EmberValidations from 'ember-validations';
+
+var validatableProxy = function(key, validations) {
+  return Ember.computed(function() {
+    return Ember.ObjectProxy.createWithMixins(
+      BufferedMixin,
+      EmberValidations.Mixin, {
+      content: this.get(key),
+      validations: validations,
+      container: this.get('container'),
+      _validate: function() {
+        return this._super();
+      }
+    });
+  });
+};
+
+export default Ember.Controller.extend({
+
+  myModel: null,
+
+  form: validatableProxy('myModel', {
+    name: {
+      presence: true
+    }
+  }),
+
+  init() {
+    this.set('myModel', this.store.createRecord('foo'))
+  },
+
+  actions: {
+    validate() {
+      this.get('form').validate()
+        .then(() => {
+          this.get('form').applyChanges();
+          console.log(this.get('myModel').toJSON());
+        })
+        .catch(() => {
+          console.log('Invalid', JSON.stringify(this.get('form.errors')));
+        })
+    }
+  }
+
+});
